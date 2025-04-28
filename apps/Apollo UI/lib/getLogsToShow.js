cb40e3039b519ff9a29eb0fc8868c37eed87afef
Becca Jonas <becca@retool.com>
2025-04-28 17:02:37 +0000

    Unprotecting Apollo UI

--- apps/Apollo UI/lib/getLogsToShow.js
+++
@@ -1,9 +0,0 @@
-const filter = logsFilters.value
-switch (filter) {
-  case 'agent':
-    return getLogsByAgent.data.data
-  case 'resource':
-    return getLogsByResourceName.data.data
-  default:
-    return getLogs.data.data
-}
--- apps/Apollo UI/lib/updateSearchParams.js
+++
@@ -1,2 +0,0 @@
-getLogsCursorDirectory.setValue(new Map().set(0, undefined))
-logList.setPage(0)
